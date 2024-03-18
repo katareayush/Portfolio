1dd1d141b4affef3fd811b9caca965cd1fed3718
Ayush Katare <katareayush2005@gmail.com>
2024-03-18 17:14:37 +0530

    added another loader style

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-window.addEventListener('load', () => {
-    const loader = document.querySelector('.loader');
-    loader.classList.add('loader-hidden');
+// window.addEventListener('load', () => {
+//     const loader = document.querySelector('.loader');
+//     loader.classList.add('loader-hidden');
 
-    loader.addEventListener("transitionend", () => {
-        loader.remove(); // Remove the loader element from the DOM
-    });
-});
+//     loader.addEventListener("transitionend", () => {
+//         loader.remove(); // Remove the loader element from the DOM
+//     });
+// });
+
+
+function loaderAnimation() {
+    var loader = document.querySelector(".loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+
+    },4200)
+}
+
+loaderAnimation();
